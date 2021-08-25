9bda64aa57b5bda883d75859e927f10de54e924b
yugandharabhosale <87601266+yugandharabhosale@users.noreply.github.com>
2021-08-25 14:14:30 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,28 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+     
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      this.image = loadImage("block.png");
+    //  World.add(world, this.image);
+    }
+    display(){
+    //  console.log(this.body.speed);
+     // if(this.body.speed <3){
+      var angle = this.body.angle;
+      var pos= this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      rect(0,0,this.width, this.height);
+      pop();
+   // }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# YUGA-S-TS--2-PROJECT
+
+### Features of Stage 2:
+##### On Space button pressed, player gets a second chance to play
+##### Blocks vanish on coming in contact with my slingshot
+
+
+
+
+
